a356fc9a5094bdd8e810be8a1ca3f1fe3a4a6842
Dheerajkhatri69 <144943383+Dheerajkhatri69@users.noreply.github.com>
2023-09-15 03:10:22 -0700

    Update DheerajJS.js

--- DheerajJS.js
+++ DheerajJS.js
@@ -1,5 +1,23 @@
-var icon =document.getElementById("icon");
-icon.onclick = function(){
-        document.body.classList.toggle("dark_them");
-        icon.classList.toggle("animation_rotat");
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+    var SetTheme = document.body;
+    SetTheme.classList.toggle("dark_them");
+    icon.classList.toggle("animation_rotat");
+    
+    var theme;
+
+    if(SetTheme.classList.contains("dark_them")){
+        theme = "DARK";
+    }else{
+        theme = "LIGHT";
     }
+    
+    localStorage.setItem("PageTheme", JSON.stringify(theme));
+    
+}
+let GetTheme = JSON.parse(localStorage.getItem("PageTheme"));
+
+if(GetTheme === "DARK"){
+    document.body.classList = "dark_them";
+    icon.classList.toggle("animation_rotat");
+}
